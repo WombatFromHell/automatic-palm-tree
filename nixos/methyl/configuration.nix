6f49e3113f406b55685078cff9cd6514e8c6bc78
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-13 09:17:03 -0700

    Switch to kyber as the default ioscheduler

--- nixos/methyl/configuration.nix
+++ nixos/methyl/configuration.nix
@@ -116,6 +116,13 @@ in {
     nvidia-pm.enable = true;
     sleepfix.enable = true;
     veridian-controller.enable = true;
+
+    # use kyber as the default ioscheduler
+    udev.extraRules = ''
+      ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="kyber"
+      ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
+      ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
+    '';
   };
 
   fonts.fontconfig.useEmbeddedBitmaps = true;
