69398b98f3320aa87e4170d7eb7d6d2779ef8373
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-14 17:28:35 -0700

    Be explicit about our permitted substituters

--- nixos/methyl/configuration.nix
+++ nixos/methyl/configuration.nix
@@ -5,8 +5,18 @@
 }: let
   user = hostArgs.username;
 in {
-  nix = {
-    settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://wombatfromhell.cachix.org/"
+      "https://nix-community.cachix.org/"
+      "https://chaotic-nyx.cachix.org/"
+    ];
+    trusted-public-keys = [
+      "wombatfromhell.cachix.org-1:pyIVJJkoLxkjH/MKK1ylrrdJKPpm+aXLeD2zAqVk9lA="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    ];
   };
   nixpkgs.config.allowUnfree = true;
 
