3b5550006dd80be800a247365ca23430fef80124
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-21 13:23:02 -0700

    Fix a typo in tweaks.nix

--- nixos/methyl/modules/tweaks.nix
+++ nixos/methyl/modules/tweaks.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
+  boot.kernel.sysctl = {
+    # set sane swappiness behavior
+    "vm.swappiness" = 1;
+    "vm.watermark_boost_factor" = 0;
+    "kernel.split_lock_mitigate" = 0;
+  };
+
   # include some custom ananicy-cpp rules
   services = {
     ananicy = {
@@ -23,12 +30,5 @@
       ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
       ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
     '';
-
-    boot.kernel.sysctl = {
-      # set sane swappiness behavior
-      "vm.swappiness" = 1;
-      "vm.watermark_boost_factor" = 0;
-      "kernel.split_lock_mitigate" = 0;
-    };
   };
 }
