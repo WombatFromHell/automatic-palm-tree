c88a3ba82ad2f887883a51eee2d6b84ceb9449da
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-02-21 13:16:46 -0700

    Include sysctl settings in tweaks.nix

--- nixos/methyl/modules/tweaks.nix
+++ nixos/methyl/modules/tweaks.nix
@@ -23,5 +23,12 @@
       ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
       ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
     '';
+
+    boot.kernel.sysctl = {
+      # set sane swappiness behavior
+      "vm.swappiness" = 1;
+      "vm.watermark_boost_factor" = 0;
+      "kernel.split_lock_mitigate" = 0;
+    };
   };
 }
