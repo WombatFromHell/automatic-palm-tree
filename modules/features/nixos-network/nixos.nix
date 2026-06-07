2d0f86382b27fd8f6d6d6180660c7279084cbac9
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-06-06 20:57:09 -0600

    Add some network related tweaks to 'nixos-network'

--- modules/features/nixos-network/nixos.nix
+++ modules/features/nixos-network/nixos.nix
@@ -5,11 +5,17 @@ _: {
       wifi.backend = "iwd";
     };
     wireless.iwd = {
+      enable = true;
       settings = {
         General.Country = "US";
-        Network.EnableIPv6 = true;
-        Settings.AutoConnect = true;
+        Network.EnableIPv6 = false;
       };
     };
+  };
+
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq_codel";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.ipv6.tcp_congestion_control" = "bbr";
   };
 }
