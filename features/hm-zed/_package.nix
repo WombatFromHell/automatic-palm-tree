c08a513fa4c6a25b12b3af93d9b2b3be37a35855
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-06-20 16:42:00 -0600

    Move the pkg ver in 'hm-zed' to 'v1.7.2' rather than nightly

--- features/hm-zed/_package.nix
+++ features/hm-zed/_package.nix
@@ -19,12 +19,12 @@
   testers,
   lib,
 }: let
-  version = "1.7.2-pre";
+  version = "1.7.2";
 
   assets = {
     "x86_64-linux" = {
       url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-      sha256 = "sha256-dXF+l+UOweAWBfCRV/bT9MNG842jUeHG4bKj5+OjmTw=";
+      sha256 = "sha256-udaYeTTCoDgz9fGWUI8Vq4ho1COW3Bp2Bxhq1w1unAI=";
     };
   };
 
